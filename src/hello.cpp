7b804681d828e6d215568b1015d887bb2a8c1ee3
Mats Gustafsson <mats@mcnab.se>
2019-09-23 15:15:46 +0200

    adds an include file and a dep to arma, does not compile for rpi

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+
+#include "hello.h"
+
 int main()
 {
         std::cout << "Hello, World!" << std::endl;
+        arma::vec a = {1, 2};
+        a.print();
         return 0;
 }
